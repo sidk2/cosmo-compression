bb780a72378fb6e963d54d2575f5749792519415
Tian Qiu <tiq014@ucsd.edu>
2025-05-28 15:25:33 -0700

    update model with one more layer

--- model/google.py
+++ model/google.py
@@ -24,14 +24,18 @@ class FactorizedPrior(LightningModule):
         self,
         N,
         M,
+        kl_weight: float = 1.0,
         log_wandb: bool = True,
     ):
         super().__init__()
         self.save_hyperparameters()
         self.log_wandb = log_wandb
         self.validation_step_outputs = []
+        self.kl_weight = kl_weight
         self.encoder = nn.Sequential(
             conv(1, N),
+            GDN(N),
+            conv(N, N),
             GDN(N),
             conv(N, N),
             GDN(N),
@@ -50,15 +54,39 @@ class FactorizedPrior(LightningModule):
             GDN(N, inverse=True),
             deconv(N, N),
             GDN(N, inverse=True),
+            deconv(N, N),
+            GDN(N, inverse=True),
             deconv(N, 1),
         )
         self.N = N
         self.M = M
+        self.latent_dim = 256
+        # vae components
+        self.fc_mu = nn.Linear(self.M * 4 * 4, self.latent_dim)
+        self.fc_logvar = nn.Linear(self.M * 4 * 4, self.latent_dim)
+        self.fc_decoder = nn.Linear(self.latent_dim, self.M * 4 * 4)
 
+    def reparameterize(self, mu, logvar) -> torch.Tensor:
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps * std + mu
+    
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+        enc_out = self.encoder(x)
+        enc_out = enc_out.view(x.size(0), -1)
+        mu = self.fc_mu(enc_out)
+        logvar = self.fc_logvar(enc_out)
+        latent = self.reparameterize(mu, logvar)
+        latent = self.fc_decoder(latent)
+        # latent = nn.GELU()(latent)
+        latent = latent.view(x.size(0), self.M, 4, 4)
+        dec_out = self.decoder(latent)
+        return dec_out, mu, logvar
+
+    # def forward(self, x):
+    #     x = self.encoder(x)
+    #     x = self.decoder(x)
+    #     return x
     
     def configure_optimizers(self) -> Dict[str, Any]:
         optimizer = torch.optim.AdamW(self.parameters(), lr=5e-5)
@@ -79,6 +107,8 @@ class FactorizedPrior(LightningModule):
         loss_dict = self.get_loss(batch=batch)
         loss = loss_dict['loss']
         self.log("train/loss", loss, prog_bar=True, sync_dist=True)
+        self.log("train/recon_loss", loss_dict['recon_loss'], prog_bar=True, sync_dist=True)
+        self.log("train/kl_loss", loss_dict['kl_loss'], prog_bar=True, sync_dist=True)
         return loss
     
     def validation_step(
@@ -107,10 +137,15 @@ class FactorizedPrior(LightningModule):
                 
         # Train representation
         # the size list is for debugging
-        out = self.forward(y)
+        out, mu, logvar = self.forward(y)
         recon_loss = nn.functional.mse_loss(out, y)
-        loss = recon_loss
-        return {'loss': loss}
+        kl_loss = torch.mean(-0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp(), dim=1),
+                            dim=0
+        )
+        # beta scaling of kl loss
+        loss = recon_loss +  self.kl_weight * kl_loss
+        # loss = recon_loss
+        return {'loss': loss, 'recon_loss': recon_loss, 'kl_loss': kl_loss}
 
     @utilities.rank_zero_only
     def _log_figures(
@@ -119,7 +154,7 @@ class FactorizedPrior(LightningModule):
         log=True,
     ) -> None:
         y, cosmo = batch
-        out = self.forward(y)
+        out, _, _ = self.forward(y)
         if log:
             print("Logging")
             # plot field reconstruction
